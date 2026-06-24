3ddd8adadffa892a219b253e941ebe7627db4255
Hemant <emeehemant159@gmail.com>
2026-06-24 16:06:31 +0530

    updated package-lock.json

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,11 +1,3 @@
 import { defineConfig } from '@prisma/config'
 
-export default defineConfig({
-  earlyAccess: true,
-  migrate: {
-    schema: './prisma/schema.prisma',
-    database: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
+export default defineConfig({})
